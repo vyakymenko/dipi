87b23402964534f086ffe32e27bf05c7486a5ec2
Valentyn Yakymenko <vyakimenko@codeminders.com>
2019-08-12 17:52:28 +0300

    * wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { DipiModule } from 'dipi';
@@ -11,6 +12,7 @@ import { DipiModule } from 'dipi';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     DipiModule
   ],
   providers: [],
